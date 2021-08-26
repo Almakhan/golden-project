c9e8337dbb5c61d8d26c9c142b41041641a34b80
Almakhan <82462115+Almakhan@users.noreply.github.com>
2021-08-26 14:52:53 +0530

    Add files via upload

---
+++ instruction.js
@@ -0,0 +1,18 @@
+function instruction(){
+        textSize(30);
+        fill(63,57,49);
+        text("INSTRUCTIONS:- ", 90, 430);
+        textSize(20);
+        fill(109,121,115);
+        text("-you need to collect 100 fruit to go to your home ", 100, 460);
+        textSize(20);
+        fill(109,121,115);
+        text("-press up arrow to jump ", 100, 490);
+        textSize(20);
+        fill(109,121,115);
+        text("-you can just survive 3 times if the fox will touch you and after that you'll lose ", 100, 520);
+        textSize(20);
+        fill(109,121,115);
+        text("-you need to collect 50 fruit to go to your home ", 100, 550);
+
+}
